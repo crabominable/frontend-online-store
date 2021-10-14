a1368c5e5b7c9f4b380ce7033f3df2661163418f
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2021-10-14 14:11:19 -0300

    Updates

---
+++ precache-manifest.c08dd8c79d28261bb5d44c3a966fab8f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2ccb835c228b9d9b87a5a141293cf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aeee4d16a17ff2ab881",
+    "url": "/static/css/main.ee95545e.chunk.css"
+  },
+  {
+    "revision": "0e4870ec6437d96318fa",
+    "url": "/static/js/2.b04af1ce.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.b04af1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aeee4d16a17ff2ab881",
+    "url": "/static/js/main.570931b3.chunk.js"
+  },
+  {
+    "revision": "837674402e425cca2f8d",
+    "url": "/static/js/runtime-main.6839dbe2.js"
+  },
+  {
+    "revision": "2a687ed98902ea3e7d2410f75383af74",
+    "url": "/static/media/icone-adicionar.2a687ed9.svg"
+  },
+  {
+    "revision": "542b3133be0ee0cb13f1f47f2ef9f546",
+    "url": "/static/media/icone-cabeçalho.542b3133.svg"
+  },
+  {
+    "revision": "2253c48fe04285d73c8944e999553036",
+    "url": "/static/media/icone-deletar.2253c48f.svg"
+  },
+  {
+    "revision": "200ec5b0fc18cd1737c3a47e95f26ecd",
+    "url": "/static/media/icone-pesquisa.200ec5b0.svg"
+  },
+  {
+    "revision": "ec6a7ea22cedbbf1f896de1d35fd3e70",
+    "url": "/static/media/icone-produtos.ec6a7ea2.svg"
+  },
+  {
+    "revision": "8440f26beb969cae9c6e7f002ab8c151",
+    "url": "/static/media/icone-remover.8440f26b.svg"
+  },
+  {
+    "revision": "a1af57c30a5973b6083b70b3566d7abb",
+    "url": "/static/media/icone-voltar.a1af57c3.svg"
+  }
+]);
